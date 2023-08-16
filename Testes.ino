3b4f5c0a520f35949251e85b96695f4abd4e6121
Anderson Camargo <anderson101214@hotmail.com>
2023-08-16 16:54:21 -0300

    update

--- Testes.ino
+++ Testes.ino
@@ -1,17 +1,17 @@
 #include "dht.h" //Inclusão da biblioteca
 
 //int pinoSolenoide = 7
-const int pinoNivelAgua = 6; //Define sensor como pino 6
+//const int pinoNivelAgua = 6; //Define sensor como pino 6
 const int pinoSensorUmidadeSolo = A1; //Pino sensor ligado a porta A1
 const int pinoDHT11 = A0; //Pino sinal do sensor ligado a porta A0
 dht DHT; //Variável para fazer referencia a biblioteca DHT
-int soloSECO = 900 //A verificar, medida do solo seco
+int soloSECO = 900; //A verificar, medida do solo seco
 
 
 void setup() {
   Serial.begin(9600); // Inicia comunicação serial em 9600
   pinMode(pinoSensorUmidadeSolo, INPUT);
-  pinMode(pinoNivelAgua, INPUT)
+  //pinMode(pinoNivelAgua, INPUT)
   //pinMode(pinoSolenoide, OUTPUT)
 }
 
@@ -25,58 +25,58 @@ void loop() {
   Serial.print(DHT.temperature, 0); //Imprime o valor medido e remove a parte decimal
   Serial.println("C"); 
 
-  int umidadeSolo = analogRead(pinoSensorUmidadeSolo)
-  Serial.print(" Umidade solo: ");
+  int umidadeSolo = analogRead(pinoSensorUmidadeSolo);
+  Serial.print("| Umidade solo: ");
   Serial.print(umidadeSolo);
 
-  int estadoNivelAgua = digitalRead(sensor); // define que estado esta a boia 0 ou 1
-  Serial.print("Estado sensor : "); //Printa Estado sensor
-  Serial.println(estadoNivelAgua); //Printa a leitura de estado
+  //int estadoNivelAgua = digitalRead(sensor); // define que estado esta a boia 0 ou 1
+  //Serial.print("Estado sensor : "); //Printa Estado sensor
+  //Serial.println(estadoNivelAgua); //Printa a leitura de estado
 
-  if (estadoNivelAgua == 1) {
+  //if (estadoNivelAgua == 1) {
   //digitalWrite(pinoSolenoide, true)
-  } else {
+  //} else {
   //digitalWrite(pinoSolenoide, false)
-  }
+  //}
 
   
-  verificarUmidade()
-  verificarTemperatura()
+  verificarUmidade(umidadeSolo);
+  verificarTemperatura();
 
 }
 
 
-void verificarUmidade(){
+void verificarUmidade(int a){
 
-  if (umidadeSolo >= soloSECO) {
-  Serial.print("Solo seco VERIFICAR")
+  if (a >= soloSECO) {
+  Serial.println("| Solo seco VERIFICAR");
   //Add ação
   } else {
-  Serial.print("Umidade solo OK")
+  Serial.println("| Umidade solo OK");
   //Add ação
   }
 
-  delay(64000)
+  delay(1000);
 
 }
 
 void verificarTemperatura(){
 
   //Germinação da semente  
-  if (DHT.temperature >= 15 and DHT.temperature <= 25 ) {
-  Serial.print("Temperatura OK para germinação")
+  if ( DHT.temperature >= 15 && DHT.temperature <= 25 ) {
+  Serial.println("| Temperatura OK para germinação");
   } else {
-  Serial.print("Temperatura fora das recomendadas para germinação da semente")
+  Serial.println("| Temperatura fora das recomendadas para germinação da semente");
   //Add ação
   }
 
   //desenvolvimento e produção do tomate
   if (DHT.temperature >= 10 and DHT.temperature <= 34 ) {
-  Serial.print("Temperatura OK para desenvolvimento e produção do tomate")
+  Serial.println("| Temperatura OK para desenvolvimento e produção do tomate");
   } else {
-  Serial.print("Temperatura fora das recomendadas para desenvolvimento e produção do tomate")
+  Serial.println("| Temperatura fora das recomendadas para desenvolvimento e produção do tomate");
   //Add ação
   }
-  delay(64000)
+  delay(1000);
 
 }
