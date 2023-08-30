ee60e3f0fa9dbde8ceff197cce77b951c55b1570
Anderson Camargo <74426791+anderson1610@users.noreply.github.com>
2023-08-30 12:55:01 -0300

    Update markov.py

--- markov.py
+++ markov.py
@@ -4,32 +4,6 @@ import datetime
 import locale
 import numpy as np
 
-
-
-# definindo a matriz
-# probabilidades semana 1
-M1 = np.array([
-    [0.7, 0.1],
-    [0.3, 0.9]
-])
-
-# calculando a matriz
-# probabilidades semana 2
-M2 = np.linalg.matrix_power(M1, 2)
-
-# definindo grupo
-# semana 1
-grupo_semana1 = np.array([
-    [0.4],
-    [0.6]
-])
-
-# calculando grupo
-# semana 3
-grupo_semana3 = np.matmul(M2, grupo_semana1)
-print(grupo_semana3)
-
-
 # Definir a localização para pt-BR
 locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
 
@@ -40,14 +14,19 @@ mes_atual = datetime.datetime.now().strftime('%B').capitalize()
 excel_file = 'Santo amaro indices.xlsx'
 df = pd.read_excel(excel_file)
 
-# Acessar o valor da célula da coluna 'Agosto' e linha 22
+# Acessar o valor da célula da coluna 'Agosto' e linha 
 valor = df.at[0, f'{mes_atual}']  # 21 porque os índices de linha em Python começam em 0
 
-# Criar matrizes
-A = np.array([[valor, 2.0], [3.0, 4.0]])
-B = np.array([[5.0], [8.0]])
+# definindo a matriz de probabilidades mês x em 2023
+M1 = np.array([
+    [valor, 0],
+    [0, 0]
+])
+
+# calculando a matriz probabilidades mês x em 2024
+M2 = np.linalg.matrix_power(M1, 2)
 
 # Multiplicação de matrizes
-produto = np.dot(A, B)
+produto = np.dot(M1, M2)
 produto2 = int(produto[0][0])
 print(produto2)
