6ec9db31ca03f0a9e4835a1cc22fe5574b905e05
Anderson Camargo <74426791+anderson1610@users.noreply.github.com>
2023-09-15 13:59:24 -0300

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import mysql.connector
 import datetime
 import locale
 import numpy as np
@@ -10,25 +10,142 @@ np.set_printoptions(precision=2, suppress=True)
 locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
 
 # Obter o mês atual
-mes_atual = datetime.datetime.now().strftime('%B').capitalize()
+mes_atual = datetime.datetime.now().strftime('%B').capitalize().upper()
 
-# Ler a planilha Excel usando o pandas
-excel_file = 'Santo amaro indices.xlsx'
-df = pd.read_excel(excel_file)
+# Obtenha a data atual
+data_atual = datetime.datetime.now()
 
-# Acessar o valor da célula da coluna 'Agosto' e linha 
-valor = df.at[0, f'{mes_atual}']  # 21 porque os índices de linha em Python começam em 0
+# Extraia o ano da data atual
+ano_atual = data_atual.year
 
-# definindo a matriz de probabilidades mês x em 2023
-M1 = np.array([
-    [valor, 0],
-    [0, 0]
-])
+MATRIZES_MARKOV = {
+    #Matriz de markov referente ao mês de Janeiro definindo a probabilidades mês em 2023
+    'JANEIRO' : np.array([
+        [1, 0],
+        [0, 1]
+    ]),
 
-# calculando a matriz probabilidades mês x em 2024
-M2 = np.linalg.matrix_power(M1, 2)
+    #Matriz de markov referente ao mês de Fevereiro definindo a probabilidades mês em 2023
+    'FEVEREIRO' : np.array([
+        [0.83, 1],
+        [0.17, 0]
+    ]),
 
-# Multiplicação de matrizes
-produto = np.dot(M1, M2)
-produto2 = int(produto[0][0])
-print(M1)
+    #Matriz de markov referente ao mês de Março definindo a probabilidades mês em 2023
+    'MARCO' : np.array([
+        [0, 1],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Abril definindo a probabilidades mês em 2023
+    'ABRIL' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Maio definindo a probabilidades mês em 2023
+    'MAIO' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Junho definindo a probabilidades mês em 2023
+    'JUNHO' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Julho definindo a probabilidades mês em 2023
+    'JUNLHO' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Agosto definindo a probabilidades mês em 2023
+    'AGOSTO' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Setembro definindo a probabilidades mês em 2023
+    'SETEMBRO' : np.array([
+        [0.83, 1],
+        [0.17, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Outubro definindo a probabilidades mês em 2023
+    'OUTUBRO' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Novembro definindo a probabilidades mês em 2023
+    'NOVEMBRO' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+    #Matriz de markov referente ao mês de Dezembro definindo a probabilidades mês em 2023
+    'DEZEMBRO' : np.array([
+        [0, 0],
+        [0, 0]
+    ]),
+
+}
+
+ANO_REFERENCIA = {
+
+    '2023' : 1,
+    '2024' : 2,
+    '2025' : 3,
+    '2026' : 4,
+    '2027' : 5
+
+}
+
+def inserir_markov():
+
+    # Conectar ao banco de dados MySQL (db4free.net)
+    conn = mysql.connector.connect(
+        host='85.10.205.173', # Substitua pelo endereço IP ou nome do host do seu servidor MySQL
+        user='',   # Substitua pelo nome de usuário do MySQL
+        password='', # Substitua pela senha do MySQL
+        database='tccarduino2023'  # Substitua pelo nome do banco de dados
+    )
+
+    cursor = conn.cursor()
+
+    # Criar a tabela se ela não existir
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS Markov{ano_atual} (
+            Mes TEXT,      
+            Probabilidade FLOAT
+        )
+    ''')
+    conn.commit()
+
+    # Inserir informações no banco de dados
+    insert_query = f'''
+        INSERT INTO Markov{ano_atual} (Mes, Probabilidade)
+        VALUES (%s, %s)
+    '''
+
+    # Coleta a matriz de acordo com o mês atual
+    matriz_atual = MATRIZES_MARKOV.get(mes_atual)
+
+    # Coleta a key de referencia de acordo com o ano atual
+    referencia = ANO_REFERENCIA.get(f'{ano_atual}')
+
+    #Realiza o calculo da matriz de markov de acordo com o ano e mês atual
+    calculo = np.linalg.matrix_power(matriz_atual, referencia)
+
+    #Coleta a probabilidade do mês atual ser um mês chuvoso
+    PROBABILIDADE_CHUVA = float(calculo[0][0])
+
+    values = (mes_atual, PROBABILIDADE_CHUVA)
+    cursor.execute(insert_query, values)
+    conn.commit()
+    print("Dados inseridos no banco de dados")
+
+inserir_markov()
+
